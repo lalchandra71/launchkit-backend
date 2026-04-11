ba20c8fbb34458cdb56ca0b1d8b15311f23011e6
lalchandra <lalchandrajyoti71@gmail.com>
2026-04-11 17:31:34 +0530

    fixed webhook issue

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,14 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
   app.use(
     '/webhooks/stripe',
     bodyParser.raw({ type: 'application/json' }),
   );
-
-  const corsOptions = {
+  app.enableCors({
     origin: ["http://localhost:3000","https://launchkit-frontend-wheat.vercel.app/"],
     credentials: true,
-  };
-  console.log('CORS enabled with origins:', corsOptions.origin);
-  
-  app.enableCors(corsOptions);
+  });
   await app.listen(process.env.PORT ?? 3001);
-  console.log('Server running on port:', process.env.PORT ?? 3001);
 }
 void bootstrap();
