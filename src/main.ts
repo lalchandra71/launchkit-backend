6619f316cd2baef4016162759eff83d5e8d5e0c4
lalchandra <lalchandrajyoti71@gmail.com>
2026-04-11 17:36:37 +0530

    fixed webhook issue fixed cors error

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,11 @@ async function bootstrap() {
     bodyParser.raw({ type: 'application/json' }),
   );
   app.enableCors({
-    origin: ["http://localhost:3000","https://launchkit-frontend-wheat.vercel.app/"],
+    origin: ["http://localhost:3000","https://launchkit-frontend-wheat.vercel.app"],
     credentials: true,
   });
+  console.log('CORS enabled for:', ["http://localhost:3000","https://launchkit-frontend-wheat.vercel.app"]);
   await app.listen(process.env.PORT ?? 3001);
+  console.log('Server running on port:', process.env.PORT ?? 3001);
 }
 void bootstrap();
